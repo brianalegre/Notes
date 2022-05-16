9137922d6c0c2b36adab2d0155861ab27b0fe296
Brian Alegre <brialegre@yahoo.com>
2022-05-16 14:56:52 -0700

    Added notes on Arrow Function Syntax

---
+++ NodeJS/arrowFunction.js
@@ -0,0 +1,20 @@
+// Arrow Function Syntax
+// A quicker way to write functions
+
+// Typical Function
+function myFunction() {
+	console.log('Typical Function');
+}
+
+// Invoke Typical Function
+myFunction();
+// Displays
+// Typical Function
+
+// Arrow Function
+let myArrow = () => console.log('Arrow Function');
+
+// Invoke Arrow Function
+myArrow();
+// Displays
+// Arrow Function
