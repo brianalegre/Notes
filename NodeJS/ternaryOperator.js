6da4a1260801235365635fe18e343507c85fe800
Brian Alegre <brialegre@yahoo.com>
2022-05-16 21:15:25 -0700

    Added notes for ternaryOperator

---
+++ NodeJS/ternaryOperator.js
@@ -0,0 +1,31 @@
+// Ternary Operator is the shorthand for IF statements
+
+// Typical IF statement
+
+let num1 = 1;
+let num2 = 1;
+if (num1 === num2) {
+	console.log('Numbers are the same');
+} else {
+	console.log('Numbers are different');
+}
+
+// Displays:
+// Numbers are the same
+
+// Tenary Operator
+num1 === num2
+	? console.log('Numbers are the same')
+	: console.log('Numbers are different');
+
+// Displays:
+// Numbers are the same
+
+// ? - is your IF
+// After the ? is the true section
+// : - is your ELSE
+// After the : is your false section
+
+// Structure, 3 PARTS separated by ? and :
+// CONDITION ? IF TRUE DO THIS : ELSE DO THIS
+//
